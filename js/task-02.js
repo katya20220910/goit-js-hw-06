1bcd3bb3bb746d1a692b0b75021018235411e6c5
katya20220910 <katyaantonova009@gmail.com>
2022-10-22 22:06:36 +0300

    UpTask2

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const list = document.querySelector(#ingredients);
+
+
+const list = document.querySelector("#ingredients");
+const listArr = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  
+  const ingredient = document.createElement("li");
+  ingredient.textContent = ingredients[i];
+  ingredient.classList.add("item");
+  
+  console.log(ingredient);
+  listArr.push(ingredient);
+}
+list.append(...listArr);
