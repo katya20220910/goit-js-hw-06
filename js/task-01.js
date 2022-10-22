854cd9955e9539e10eab429d229f3a12c84322e1
katya20220910 <katyaantonova009@gmail.com>
2022-10-22 17:32:18 +0300

    upd

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const navElement = document.querySelector('#categories');
+const listItem = navElement.querySelectorAll('.item');
+console.log(`Number of categories: ${listItem.length}`);
+console.log("");
+const nameItem = document.querySelectorAll('.item');
