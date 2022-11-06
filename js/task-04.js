8175da964ffa53ec7db8b3037944368639adb6d8
katya20220910 <katyaantonova009@gmail.com>
2022-11-06 19:42:56 +0200

    Update 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,21 @@
 let counterValue = 0;
+
 const buttonDec = document.querySelector('[data-action="decrement"]');
-const btnDecrement = () => {
+const buttonInc = document.querySelector('[data-action="increment"]');
+const decrement = () => {
     counterValue -= 1;
+    document.getElementById('value').textContent = counterValue;
 };
-buttonDec.addEventListener("click", btnDecrement);
+
+const increment = () => {
+    counterValue += 1;
+    document.getElementById('value').textContent = counterValue;
+};
+
+buttonDec.addEventListener("click", decrement);
+buttonInc.addEventListener("click", increment);
+
+
+console.log(counterValue);
+console.log(buttonDec);
+console.log(buttonInc);
